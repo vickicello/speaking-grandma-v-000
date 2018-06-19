dc5d66c68934ad5e735f1e8ac4491dcd2d7b8542
Vicki Hamm <vicki.cello@gmail.com>
2018-06-19 15:49:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -8,12 +8,12 @@ def speak_to_grandma(phrase)# Write a speak_to_grandma method.
   end
 end
 
-    
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
